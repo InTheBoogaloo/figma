5c1f139b0d7925d4fd0bd004b76131969c3eb61f
InTheBoogaloo <cuentagenerica5467@gmail.com>
2026-05-15 20:05:09 -0600

    salto a car details

--- lib/widgets/cars/car_card.dart
+++ lib/widgets/cars/car_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../screens/car_details_screen.dart';
 import '../models/car_models.dart';
 
 class CarCard extends StatelessWidget {
@@ -8,37 +9,45 @@ class CarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 10,
-            color: Colors.black.withOpacity(0.05),
-            offset: const Offset(0, 5),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Image.network(car.image, fit: BoxFit.contain),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            car.name,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-          Text(car.brand, style: const TextStyle(color: Colors.grey)),
-          const Spacer(),
-          Text(
-            "\$${car.price}/day",
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const CarDetailsScreen()),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              color: Colors.black.withOpacity(0.05),
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Image.network(car.image, fit: BoxFit.contain),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              car.name,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            Text(car.brand, style: const TextStyle(color: Colors.grey)),
+            const Spacer(),
+            Text(
+              "\$${car.price}/day",
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ],
+        ),
       ),
     );
   }
