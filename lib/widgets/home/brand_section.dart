369e8090562c1a1e455108105158dfaae58c3353
InTheBoogaloo <cuentagenerica5467@gmail.com>
2026-05-15 20:08:28 -0600

    salto a brands section

--- lib/widgets/home/brand_section.dart
+++ lib/widgets/home/brand_section.dart
@@ -1,36 +1,31 @@
 import 'package:flutter/material.dart';
+import '../../screens/brands_screen.dart';
 
 class BrandSection extends StatelessWidget {
   const BrandSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final brands = [
       {
         "name": "Tesla",
-        "logo":
-            "https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png"
+        "logo": "https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png"
       },
       {
         "name": "Mercedes",
-        "logo":
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/500px-Mercedes-Logo.svg.png"
+        "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/500px-Mercedes-Logo.svg.png"
       },
       {
         "name": "Ferrari",
-        "logo":
-            "https://i.pinimg.com/736x/41/d2/e5/41d2e5e80479f2185c9d1a9d2bf3c66e.jpg"
+        "logo": "https://i.pinimg.com/736x/41/d2/e5/41d2e5e80479f2185c9d1a9d2bf3c66e.jpg"
       },
       {
         "name": "Bugatti",
-        "logo":
-            "https://cdn.freebiesupply.com/logos/large/2x/bugatti-3-logo-png-transparent.png"
+        "logo": "https://cdn.freebiesupply.com/logos/large/2x/bugatti-3-logo-png-transparent.png"
       },
       {
         "name": "BMW",
-        "logo":
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/3840px-BMW.svg.png"
+        "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/3840px-BMW.svg.png"
       },
     ];
 
@@ -42,24 +37,25 @@ class BrandSection extends StatelessWidget {
           children: [
             const Text(
               "Trending Brands",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const BrandsScreen()),
+                );
+              },
               child: const Text(
                 "View All",
                 style: TextStyle(color: Colors.black54),
               ),
-            )
+            ),
           ],
         ),
 
         const SizedBox(height: 15),
 
-        /// BRAND LIST
         SizedBox(
           height: 100,
           child: ListView.separated(
@@ -68,7 +64,6 @@ class BrandSection extends StatelessWidget {
             separatorBuilder: (_, __) => const SizedBox(width: 14),
             itemBuilder: (_, index) {
               final brand = brands[index];
-
               return Column(
                 children: [
                   Container(
@@ -86,18 +81,12 @@ class BrandSection extends StatelessWidget {
                         )
                       ],
                     ),
-                    child: Image.network(
-                      brand["logo"]!,
-                      fit: BoxFit.contain,
-                    ),
+                    child: Image.network(brand["logo"]!, fit: BoxFit.contain),
                   ),
                   const SizedBox(height: 6),
                   Text(
                     brand["name"]!,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: Colors.black54,
-                    ),
+                    style: const TextStyle(fontSize: 12, color: Colors.black54),
                   )
                 ],
               );
